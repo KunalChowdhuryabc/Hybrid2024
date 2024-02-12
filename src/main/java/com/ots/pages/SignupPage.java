e03a3ce6f2563e2aad90826e581d69bf45992119
kunal <kunal@192.168.1.42>
2024-02-12 20:39:42 +0530

    1st

---
+++ src/main/java/com/ots/pages/SignupPage.java
@@ -0,0 +1,16 @@
+package com.ots.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SignupPage 
+{
+WebDriver driver;
+	
+	public SignupPage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+	
+
+}
